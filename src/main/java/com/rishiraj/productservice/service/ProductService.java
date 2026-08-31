ce9f59a095b77204935c0b1583eca4f99a204854
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2026-08-31 15:33:23 +0530

    Added put delete and getAll mapping for 3rd party API of fakeStoreDB

--- src/main/java/com/rishiraj/productservice/service/ProductService.java
+++ src/main/java/com/rishiraj/productservice/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.rishiraj.productservice.service;
 
+import com.rishiraj.productservice.dto.FakeStoreProductDto;
 import com.rishiraj.productservice.model.Product;
 
 import java.util.List;
 
 public interface ProductService {
     Product getSingleProduct(Long productId);
-    List<Product> getAllProducts();
-    Product CreateProduct(Product product);
+    List<FakeStoreProductDto> getAllProducts();
+    Product createProduct(Product product);
+    void deleteProduct(Long productId);
+    FakeStoreProductDto updateProduct(Long productId, FakeStoreProductDto product);
 
 }
