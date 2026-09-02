6fffa25eb5956bb1d3ca5c355eadf4835fbce40c
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2026-09-02 14:11:59 +0530

    Added Base Model and Created table in database using hibernate annotaion @Entity and learned how to interacts with database with the help of hibernate without writing any sql queries

--- src/main/java/com/rishiraj/productservice/model/Category.java
+++ src/main/java/com/rishiraj/productservice/model/Category.java
@@ -1,15 +1,22 @@
 package com.rishiraj.productservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Category {
-    private Long id;
+@Entity
+public class Category extends BaseModel{
+
     private String title;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
---
+++ src/main/java/com/rishiraj/productservice/repository/CategoryRepo.java
@@ -0,0 +1,8 @@
+package com.rishiraj.productservice.repository;
+
+import com.rishiraj.productservice.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepo extends JpaRepository<Category, Long> {
+
+}
