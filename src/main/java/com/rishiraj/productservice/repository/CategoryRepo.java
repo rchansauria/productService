2fc69442d1a345c58567f79d5ccca3611794d883
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2026-09-03 14:57:07 +0530

    Implement self product service rather than using third party FakeStoreService and with the help of @Qualifire annotation clears the spring that inject SelfProductService class and not FakeStoreProductService and in service class i injected Product and category Repositories that help to inject product in database.

--- src/main/java/com/rishiraj/productservice/repository/CategoryRepo.java
+++ src/main/java/com/rishiraj/productservice/repository/CategoryRepo.java
@@ -4,5 +4,5 @@ import com.rishiraj.productservice.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
-
+            Category findByTitle(String title);
 }
--- src/main/java/com/rishiraj/productservice/service/FakeStoreProductService.java
+++ src/main/java/com/rishiraj/productservice/service/FakeStoreProductService.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+@Service("FakeProductStoreService")
 public class FakeStoreProductService implements ProductService{
 
     private RestTemplate restTemplate;
